9beebd36df32c0a9428f524f544a50e5fda52666
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2025-02-17 12:07:21 +0000

    Started Welcome Screen

---
+++ lib/View/Login/welcome_screen_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreenView extends StatefulWidget {
+  const WelcomeScreenView({super.key});
+
+  @override
+  State<WelcomeScreenView> createState() => _WelcomeScreenViewState();
+}
+
+class _WelcomeScreenViewState extends State<WelcomeScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+      
+    );
+  }
+}
