3e52db93b5cf6b7579264da539c05a42fd7519e5
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2025-02-17 15:06:25 +0000

    Welcome Screen Update/Added some widgets

--- lib/View/Login/welcome_screen_view.dart
+++ lib/View/Login/welcome_screen_view.dart
@@ -1,3 +1,4 @@
+import 'package:elaros_gp4/Widgets/text_style_black.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreenView extends StatefulWidget {
@@ -10,42 +11,69 @@ class WelcomeScreenView extends StatefulWidget {
 class _WelcomeScreenViewState extends State<WelcomeScreenView> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-              child: Row(
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.yellow[200],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(height: 200,),
               Container(
-                child: Text('Sleepy Fox'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 200,
+                      child: Center(
+                        child: Column(
+                          children: [
+                            StrokeText(
+                              text: "Sleepy",
+                              textStyle: TextStyle(fontSize: 70),
+                            ),
+                            StrokeText(
+                              text: "Fox",
+                              textStyle: TextStyle(fontSize: 70),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(height: 0),
               Container(
-                child: Text('Sleepy Fox'),
+                color: Colors.white10,
+                decoration: DecoratedBox(),
+                child: Column(
+                  children: [
+                    Text(
+                      'Quick Guide',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: Text('Guide example'),
+                    ),
+                    SizedBox(height: 100),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(onPressed: () {}, child: Text("Back")),
+                        ElevatedButton(onPressed: () {}, child: Text("Next")),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
-          )
           ),
-          Expanded(
-            child: Container(
-              child: Column(
-                children: [
-                  Text('Quick Quide'),
-                  Container(
-                    child: Text('Quide example'),
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(onPressed: (){}, child: Text("Back")),
-                      ElevatedButton(onPressed: (){}, child: Text("Next"))
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
-      
     );
   }
 }
