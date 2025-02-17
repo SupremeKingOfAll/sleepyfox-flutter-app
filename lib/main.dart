ab94c3701e602769ecafd663df3f1f556b166d5d
qwertmus <c3025664@hallam.shu.ac.uk>
2025-02-17 11:51:16 +0000

    dashboard basic flex layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'View/Dashboard/dashboard_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
