ae4e5ae0a05e48c3ae262b772cf2547d2baa8f50
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2025-02-18 16:46:39 +0000

    Quick login fix

--- lib/View/Login/login_view.dart
+++ lib/View/Login/login_view.dart
@@ -30,71 +30,73 @@ class LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 249, 217, 130),
-      body: Column(
-        children: [
-          const SizedBox(height: 100),
-          Image.asset('Assets/SleepyFoxLogo512.png', width: 150, height: 150),
-          const SizedBox(height: 35),
-          const Text(
-            'Welcome to Sleepy Fox',
-            style: TextStyle(fontSize: 30),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            Image.asset('Assets/SleepyFoxLogo512.png', width: 150, height: 150),
+            const SizedBox(height: 35),
+            const Text(
+              'Welcome to Sleepy Fox',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
-                prefixIcon: Icon(Icons.email),
-                filled: true,
-                fillColor: Colors.white,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
-                prefixIcon: Icon(Icons.lock),
-                filled: true,
-                fillColor: Colors.white,
-              ),
-              obscureText: true,
-            ),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: _handleLogin,
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(200, 50),
-              side: const BorderSide(color: Colors.black, width: 0.5),
-            ),
-            child: const Text(
-              'Login',
-              style: TextStyle(fontSize: 17),
-            ),
-          ),
-          const SizedBox(height: 35),
-          GestureDetector(
-            onTap: () => Navigator.pushReplacementNamed(context, '/Register'),
-            child: const Text(
-              'Don\'t have an account? Sign Up',
-              style: TextStyle(
-                color: Colors.blue,
-                decoration: TextDecoration.underline,
+                obscureText: true,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: _handleLogin,
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(200, 50),
+                side: const BorderSide(color: Colors.black, width: 0.5),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            const SizedBox(height: 35),
+            GestureDetector(
+              onTap: () => Navigator.pushReplacementNamed(context, '/Register'),
+              child: const Text(
+                'Don\'t have an account? Sign Up',
+                style: TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
