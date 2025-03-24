76744d157a2d092e7e053a0b37b66a401a8f91f3
Zak0100 <127435867+Zak0100@users.noreply.github.com>
2025-03-24 14:31:21 +0000

    Made education page dark theme

--- lib/Widgets/education_container.dart
+++ lib/Widgets/education_container.dart
@@ -9,28 +9,47 @@ class InfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 255, 206, 60),
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              content,
-              style: TextStyle(fontSize: 14, color: Colors.black87),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 10, 12, 24),
+            Color.fromARGB(255, 19, 23, 45),
+            const Color.fromARGB(255, 40, 33, 121)
           ],
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: Card(
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        color: Colors.transparent, // Make the card background transparent
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 209, 169, 49),
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                content,
+                style: TextStyle(
+                    fontSize: 14, color: Color.fromARGB(255, 255, 231, 158)),
+              ),
+            ],
+          ),
         ),
       ),
     );
