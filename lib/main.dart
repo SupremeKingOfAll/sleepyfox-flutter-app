889ad0e10884448de4cdfdee61a06b332aa3fbcd
qwertmus <c3025664@hallam.shu.ac.uk>
2025-03-25 15:09:44 +0000

    reverted main.dart due to version issue + index 0 on tracking now also stores sharecode in database

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DashboardView(),
+      home: const LoginPage(),
       routes: {
         '/EducationView': (context) => const EducationView(),
         '/AboutUs': (context) => const AboutUs(),
