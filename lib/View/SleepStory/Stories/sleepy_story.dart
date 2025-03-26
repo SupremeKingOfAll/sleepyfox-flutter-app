83bf1e91b962f6d15732b3c606947f7abfba02a8
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2025-03-26 14:40:19 +0000

    Story function has been created but not finished yet

---
+++ lib/View/SleepStory/Stories/sleepy_story.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SleepyStory extends StatelessWidget {
+  const SleepyStory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/View/SleepStory/sleep_story_view.dart
+++ lib/View/SleepStory/sleep_story_view.dart
@@ -1,4 +1,10 @@
+import 'package:elaros_gp4/Widgets/Text%20Styles/zaks_personal_text_style.dart';
 import 'package:flutter/material.dart';
+
+import '../../Widgets/custom_bottom_nav_bar.dart';
+import '../Dashboard/dashboard_view.dart';
+import '../Settings/settings_view.dart';
+import '../Sleep Tracker/sleep_tracker_view.dart';
 
 class SleepStoryView extends StatefulWidget {
   const SleepStoryView({super.key});
@@ -8,15 +14,41 @@ class SleepStoryView extends StatefulWidget {
 }
 
 class _SleepStoryViewState extends State<SleepStoryView> {
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SleepTracking()),
+      );
+    } else if (index == 1) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => DashboardView()),
+      );
+    } else if (index == 2) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SettingsView()),
+      );
+    } else if (index != 2) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
           color: Colors.amber.shade500,
         ),
         backgroundColor: Color.fromARGB(255, 0, 0, 0),
-        title: Text("Profiles",
+        title: Text(
+          "Profiles",
           style: TextStyle(
             color: const Color.fromARGB(
                 255, 252, 174, 41), // Light amber for the subtitle text
@@ -27,10 +59,12 @@ class _SleepStoryViewState extends State<SleepStoryView> {
             padding: EdgeInsets.only(right: 16),
             child: Align(
               alignment: Alignment.centerRight,
-              child: Text("Sleepy fox",                style: TextStyle(
-                color: const Color.fromARGB(
-                    255, 252, 174, 41), // Light amber for the subtitle text
-              ),
+              child: Text(
+                "Sleepy fox",
+                style: TextStyle(
+                  color: const Color.fromARGB(
+                      255, 252, 174, 41), // Light amber for the subtitle text
+                ),
               ),
             ),
           ),
@@ -40,13 +74,81 @@ class _SleepStoryViewState extends State<SleepStoryView> {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image:
-            AssetImage('Assets/900w-xy8Cv39_lA0.png'), // Background image
+                AssetImage('Assets/900w-xy8Cv39_lA0.png'), // Background image
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/TheSleepyStory');
+                },
+                child: Card(
+                  elevation: 12,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  shadowColor: Colors.black.withOpacity(0.4),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 23, 28, 55),
+                          Colors.blueGrey.shade700,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    width: double.infinity,
+                    height: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 28),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "The sleepy fox story",
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.amber,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              'Assets/FoxMascProfPic.png',
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
